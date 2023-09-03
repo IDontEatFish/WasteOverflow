9c731cabba0c7f789c5c0380c6d2275283c39a6b
forkiron <137863212+forkiron@users.noreply.github.com>
2023-09-03 02:13:28 -0400

    clean up

--- Frontend/main.py
+++ Frontend/main.py
@@ -6,24 +6,16 @@ from influxdb import InfluxDBClient
 from influxdb_client import InfluxDBClient
 import random 
 
-
-
 url = 'http://192.168.254.60:8086'
 token = 'vHGp8PcbtpTQVmlplzQMecvZdfpSJcUNT1wOvBXXRyOzeWmW0AsOQMWMRijSoEKuwxlURtrhael_rAD9qfPUMw=='
 org = 'ad3830e63761906d'
 bucket = 'nikolaus'
 
-data = {'Latitude': [],
-        'Longitude': [],
-        'percent': [],
+data = {'Latitude': [43.474960, 43.469070, 43.493980],
+        'Longitude': [-80.529678, -80.575690, -80.528820],
+        'percent': [random.randint(0, 100) for placeholder in range(3)],
         }
-fakedata = {
-   'Latitude': [43.474960, 43.469070, 43.493980],
-   'Longitude': [-80.529678, -80.575690, -80.528820],
-   'percent': [random.randint(0, 100) for placeholder in range(3)]
-   
-}
-#refresh 5s delay 
+
 with InfluxDBClient(url=url, token=token, org=org) as client:
     query_api = client.query_api()
 
@@ -40,28 +32,21 @@ with InfluxDBClient(url=url, token=token, org=org) as client:
                data["Longitude"] += [value]
             elif field == 'percent':
                data["percent"].append(int(value * 100))
-                           
-            
-
-print(data)
+                                   
 df = pd.DataFrame(data)
 
 df['combinedtext'] = (df['percent'].astype(str) + '%')
 
-
-
-
 with open('stream.css') as f:
   st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-#Points size
-pointcolor = 'yellow'
 st.title('WasteOverflow')
+
+pointcolor = 'yellow'
 
 image = Image.open('images/boxlogo.png')
 
 st.image(image, width=40)
-# use px.scatter_mapbox to 
 fig = px.scatter_mapbox(
     df,
     lat="Latitude",
@@ -77,12 +62,12 @@ fig.update_traces(
 
 fig.update_traces(
     marker=dict(
-        size=25,  # Set the size of the markers
-        sizemode="diameter",  # "diameter" sets the marker size as a diameter
-        sizemin=5,  # Minimum marker size
+        size=25, 
+        sizemode="diameter",  
+        sizemin=5,  
     )
 )
-# Set the Mapbox token
+
 fig.update_layout(
   mapbox=dict(accesstoken="pk.eyJ1Ijoic2Zpc2NobWUiLCJhIjoiY2xtMW5jdG40MzVwdjNrcGlkYTQ2eGY1eCJ9.kNwZdRqcAfU4lhIQn_7jnQ"),
   width=600,
@@ -93,14 +78,8 @@ fig.update_layout(
 if st.button("Refresh"):
   st.markdown("<script type='text/javascript'>window.location.reload(true);</script>", unsafe_allow_html=True)
 
-if df['percent'].max() < 80:
-    pointcolor = 'green'
-else:
-    pointcolor = 'red'
+df['marker_color'] = df['percent'].apply(lambda x: 'red' if x >= 80 else 'green')
 
-# Update the color of the map markers
-fig.update_traces(marker=dict(color=pointcolor))
+fig.update_traces(marker=dict(color=df['marker_color'], size=25, sizemode="diameter", sizemin=5))
    
-
-# Display the map
 st.plotly_chart(fig)
